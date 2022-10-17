d63c18636cda9e158972cce84d92c66ba955ce46
Neil Sarkar <neil.r.sarkar@gmail.com>
2022-10-17 13:05:26 -0700

    Add renderer for pong

--- examples/src/pong.ts
+++ examples/src/pong.ts
@@ -2,15 +2,17 @@ import {GameLoop} from './GameLoop';
 import {Key} from './enums';
 import { KeyboardSnapshot } from './KeyboardSnapshot';
 
-const leftPaddle = {
+let leftPaddle = {
+	div: document.querySelector('.left.paddle') as HTMLDivElement,
 	x: -100,
 	y: 0
-}
+};
 
-const rightPaddle = {
+let rightPaddle = {
+	div: document.querySelector('.right.paddle') as HTMLDivElement,
 	x: 100,
 	y: 0
-}
+};
 
 const gameLoop = new GameLoop(tick);
 gameLoop.run();
@@ -45,4 +47,11 @@ function tick(frame: bigint, keyboard: KeyboardSnapshot ) {
 	if (frame % 60n == 0n) {
 		console.log(leftPaddle, rightPaddle);
 	}
+
+	render()
+}
+
+function render() {
+	leftPaddle.div.style.transform = `translate(${leftPaddle.x}px, ${leftPaddle.y}px)`;
+	rightPaddle.div.style.transform = `translate(${rightPaddle.x}px, ${rightPaddle.y}px)`;
 }
