429f2a6f0a4b62f99f6002c35356790b1473f0a8
Neil Sarkar <neil.r.sarkar@gmail.com>
2022-10-17 11:25:27 -0700

    Add KeyboardSnapshot with isKeyDown, isKeyPressed, isKeyUp

---
+++ examples/src/KeyboardSnapshot.ts
@@ -0,0 +1,35 @@
+import { Key } from "./enums";
+import { Pog } from "./types";
+
+const BUFFER_LENGTH = 10;
+
+export class KeyboardSnapshot {
+	inputs: Pog.KeyboardInput[];
+
+	constructor() {
+		this.inputs = [];
+	}
+
+	addInput(input: Pog.KeyboardInput) : void {
+		// maintains a BUFFER_LENGTH lifo queue
+		this.inputs.unshift(input);
+		this.inputs = this.inputs.slice(0, BUFFER_LENGTH);
+	}
+
+	isKeyDown(key: Key): boolean {
+		return this.isKey(key, 0) && !this.isKey(key, 1);
+	}
+
+	isKeyUp(key: Key): boolean {
+		return !this.isKey(key, 0) && this.isKey(key, 1);
+	}
+
+	isKeyPressed(key: Key): boolean {
+		return this.isKey(key, 0);
+	}
+
+	isKey(key: Key, pastFrame: number = 0): boolean {
+		if (this.inputs.length <= pastFrame) { return false; }
+		return this.inputs[pastFrame].keys.indexOf(key) != -1
+	}
+}
