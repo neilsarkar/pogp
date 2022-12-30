3b49f7e48977308cc95428ed1c808d2cfe7a7b4e
Neil Sarkar <neil.r.sarkar@gmail.com>
2022-12-30 14:38:04 -0800

    Export InputType and ButtonPosition as enums

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -2,6 +2,6 @@ export {GameLoop} from "./GameLoop";
 export {GamepadSnapshot} from "./GamepadSnapshot";
 export {KeyboardSnapshot} from "./KeyboardSnapshot";
 export {MarshalInput} from "./MarshalInput";
-export { Hand, Key } from "./enums";
+export {Hand, Key, InputType, ButtonPosition} from "./enums";
 
 export {clamp} from "./lib";
